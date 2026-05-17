45d5cdfd7f91f5a3028cbf0eb90c1f26a81e786c
Berkant392 <mertrecberogullari@gmail.com>
2026-05-17 23:26:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-// src/main.jsx (GÜNCEL HALİ)
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode> KALDIRILDI!
     <App />
-  // </React.StrictMode> KALDIRILDI!
 )
